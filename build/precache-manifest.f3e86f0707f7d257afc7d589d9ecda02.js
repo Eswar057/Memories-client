7d5cc1dbbd2a4e19d4471b43ea0dafdf236e4fae
Eswar057 <eswar241201@gmail.com>
2021-07-28 12:03:41 +0530

    MEMORIES

---
+++ build/precache-manifest.f3e86f0707f7d257afc7d589d9ecda02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8808b1890db015bcbd81cd41dd2d24f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "babf339c3e6dd5b280ec",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "94722200f2bb2c54f87b",
+    "url": "/static/js/2.32d91e32.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.32d91e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babf339c3e6dd5b280ec",
+    "url": "/static/js/main.86edaf3f.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
